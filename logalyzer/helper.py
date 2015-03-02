8221c0f93597643888d2491c3232ef2b510d6971
bison-- <mike.bison42@googlemail.com>
2015-03-02 18:48:43 +0100

    argh
    forget debug

--- logalyzer/helper.py
+++ logalyzer/helper.py
@@ -1,6 +1,6 @@
 import datetime
 def getFileDate(logFileName=''):
-	print logFileName
+	#print logFileName
 	fileDate = None
 	if logFileName == 'latest.log':
 		fileDate = datetime.datetime.strptime('2999-12-12', '%Y-%m-%d')
@@ -11,9 +11,9 @@ def getFileDate(logFileName=''):
 			logFileName = logFileName[0:-9]
 		else:
 			return datetime.datetime.strptime('2999-12-12', '%Y-%m-%d')
-		print logFileName
+		#print logFileName
 		fileDateParts = logFileName.split('-')
 		fileDateString = '{0}-{1}-{2} {3}'.format(fileDateParts[0], fileDateParts[1], fileDateParts[2], fileDateParts[3])
-		print fileDateString
+		#print fileDateString
 		fileDate = datetime.datetime.strptime(fileDateString, '%Y-%m-%d %H')
 	return fileDate
